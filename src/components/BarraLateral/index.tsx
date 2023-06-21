e94f83ba26c9712cd56a7761b220270c20c5e761
flavioaluis <flavio.aluis@gmail.com>
2023-06-21 15:19:19 -0300

    Atualizando sidebar

--- src/components/BarraLateral/index.tsx
+++ src/components/BarraLateral/index.tsx
@@ -9,9 +9,9 @@ function BarraLateral() {
         <img src="/src/assets/img.jpeg" alt="foto flávio"></img>
     </div>
     <div className="Attributes">
-        <h2> Abilities </h2>
+        <h1> Abilities </h1>
             <div>
-                <h3>HTML</h3>
+                <h4>HTML</h4>
                 <span className={"values"}>0%</span><span className={"valores"}>100%</span>
                 <small>Current Range: 100</small>
                 <Slider className={"slider"}/>
