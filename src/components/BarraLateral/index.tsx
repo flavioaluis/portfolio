17b09f1a530c958a7ed3e0579713c79c8ddcb221
flavioaluis <flavio.aluis@gmail.com>
2023-06-21 15:19:54 -0300

    Atualizando sidebar

--- src/components/BarraLateral/index.tsx
+++ src/components/BarraLateral/index.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
+import Imagem from '../../assets/image.jpg';
+import style from "./BarraLateral.module.scss";
 import Slider from 'react-slider';
+
 
 
 function BarraLateral() {
     return(
     <>
-    <div>
-        <img src="/src/assets/img.jpeg" alt="foto flávio"></img>
+    
+    <div className={style.Attributes}>
+        <div className={style.imgBlock}>
+        <img width={300}  src={Imagem} alt="Flávio Foto"/>
+        </div>
+
+        <h1 className={style.title}> Desenvolvedor Full Stack </h1>
+
+        <h1 className={style.Abilities}> Abilities </h1>
+            
+            <h3 className={style.rangeA}>HTML</h3>
+            <span className={"values"}>0%</span><span className={"valores"}>100%</span>
+            <small>Current Range: 100</small>
+            <Slider className={"slider"}/>
+
+            <h3 className={style.rangeA}>Css</h3>
+            <span className={"values"}>0%</span><span className={"valores"}>100%</span>
+            <small>Current Range: 100</small>
+            <Slider className={"slider"}/>
+
+            <h3 className={style.rangeA}>Javascript</h3>
+            <span className={"values"}>0%</span><span className={"valores"}>100%</span>
+            <small>Current Range: 100</small>
+            <Slider className={"slider"}/>
+
+            <h3 className={style.rangeA}>React</h3>
+            <span className={"values"}>0%</span><span className={"valores"}>100%</span>
+            <small>Current Range: 100</small>
+            <Slider className={"slider"}/>
+
+                
+            
+            
+            
     </div>
-    <div className="Attributes">
-        <h1> Abilities </h1>
-            <div>
-                <h4>HTML</h4>
-                <span className={"values"}>0%</span><span className={"valores"}>100%</span>
-                <small>Current Range: 100</small>
-                <Slider className={"slider"}/>
-            </div>
-            <div></div>
-    </div>
+
+    
+    
     </>
 )}
 
