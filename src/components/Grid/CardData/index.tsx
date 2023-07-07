18a9ee7558908b86d0f694edb81274396d220df1
flavioaluis <flavio.aluis@gmail.com>
2023-07-07 16:03:33 -0300

    Att quadros

--- src/components/Grid/CardData/index.tsx
+++ src/components/Grid/CardData/index.tsx
@@ -1,34 +1,36 @@
-import Trabalho1  from '../../../assets/teste.png';
+import aStudies  from '../../../assets/aStudies.png';
 import Tracker from '../../../assets/Tracker.png';
+import formNegocio from '../../../assets/formNegocios.png';
+import teste from '../../../assets/teste.png';
 
 const CardData = [
     {
         img: Tracker,
-        title: "Tracker (Vue)",
+        title: "Program Alura Tracker (Vue)",
         description:"Um programa para marcar o horário de estudo ou exercícios ",
         link:"https://a-track-flavioaluis.vercel.app/",
         link2:"https://github.com/flavioaluis/aTrack",
     },
 
     {
-        img: Trabalho1,
-        title: "Trabalho2",
-        description:"LoremLoremLoremLoremLorem",
-        link:"",
-        link2:"",
+        img: aStudies,
+        title: "Program Alura Studies (React)",
+        description:"Um programa para marcar horários de estudos para o dia",
+        link:"https://alura-studies-flax-gamma.vercel.app/",
+        link2:"https://github.com/flavioaluis/aStudies",
     },
 
     {
-        img: Trabalho1,
-        title: "Trabalho3",
-        description:"LoremLoremLoremLorem",
-        link:"",
-        link2:"",
+        img: formNegocio,
+        title: "Form de Negociação (Typescript)",
+        description:"Formulário de negócios com data quantidade e valor",
+        link:"https://form-negocios.vercel.app/",
+        link2:"https://github.com/flavioaluis/formNegocios",
     },
 
     {
-        img: Trabalho1,
-        title: "Trabalho4",
+        img: teste,
+        title: "Adivinhação e Jogo da Forca (Python)",
         description:"LoremLoremLoremLoremLorem",
         link:"",
         link2:"",
