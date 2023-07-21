640d8149d8921ac0fbecaaa2c7e289cc9456ecd8
flavioaluis <flavio.aluis@gmail.com>
2023-07-21 10:01:56 -0300

    adding bootstrap in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import style from './Footer.module.scss';
 
 function Footer() {
     return(
-        <div className={style.foot}><h4> &copy; {new Date().getFullYear()} Flávio Almeida Luis |<a href="https://react.dev/">React.js</a>,<a href="https://www.typescriptlang.org/">Typescript</a> and <a href="https://sass-lang.com/">SCSS</a></h4></div>
+        <div className={style.foot}><h4> &copy; {new Date().getFullYear()} Flávio Almeida Luis |<a href="https://react.dev/">React.js</a>,<a href="https://www.typescriptlang.org/">Typescript</a>,<a href="https://getbootstrap.com/">Bootstrap</a> and <a href="https://sass-lang.com/">SCSS</a></h4></div>
 )}
 
 export default Footer;
