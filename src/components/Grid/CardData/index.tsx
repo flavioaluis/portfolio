533144b95dcb0978f04e6981afd53755c95a6fcb
flavioaluis <flavio.aluis@gmail.com>
2023-07-28 19:36:23 -0300

    att Projects

--- src/components/Grid/CardData/index.tsx
+++ src/components/Grid/CardData/index.tsx
@@ -2,6 +2,7 @@ import aStudies  from '../../../assets/ImgPortfolio/aStudies.png';
 import Tracker from '../../../assets/ImgPortfolio/Tracker.png';
 import formNegocio from '../../../assets/ImgPortfolio/formNegocios.png';
 import Aluroni from '../../../assets/ImgPortfolio/Aluroni.png';
+import Pokedex from '../../../assets/ImgPortfolio/Pokedex.png';
 
 const CardData = [
     {
@@ -30,11 +31,19 @@ const CardData = [
 
     {
         imgUrl: Aluroni,
-        title: "Aluroni (React + TypeScript)",
+        title: "Aluroni (React + TypeScript + SCSS)",
         description:"Site de um restaurante de vários tipos de refeições",
         link:"https://aluroni-ashy.vercel.app/",
         link2:"https://github.com/flavioaluis/aluroni",
     },
+
+    {
+        imgUrl: Pokedex,
+        title: "Pokedex (React + TypeScript + SCSS)",
+        description:"Site de uma pokedex usando PokeApi, um sobre, com Login, com páginas públicas e uma private ",
+        link:"https://pokedex-nine-delta-60.vercel.app/",
+        link2:"https://github.com/flavioaluis/pokedex",
+    }
 ];
 
 export default CardData;
